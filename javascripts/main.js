b29e56deba10a04605b3031940c93434064061b9
Jen Udan <you@example.com>
2016-08-14 11:19:26 -0500

    Updated JS and CSS to make expand thumbnails dynamically.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,114 +1,12 @@
 // TOGGLE SCREENSHOTS
 
-$(".thumb-1").click(function() {
-  $(".screen-1").toggle();
-  $(".thumb-1").toggle();
-});
-$(".screen-1").click(function() {
-  $(".screen-1").toggle();
-  $(".thumb-1").toggle();
-});
-$(".thumb-2").click(function() {
-  $(".screen-2").toggle();
-  $(".thumb-2").toggle();
-});
-$(".screen-2").click(function() {
-  $(".screen-2").toggle();
-  $(".thumb-2").toggle();
-});
-$(".thumb-3").click(function() {
-  $(".screen-3").toggle();
-  $(".thumb-3").toggle();
-});
-$(".screen-3").click(function() {
-  $(".screen-3").toggle();
-  $(".thumb-3").toggle();
-});
-$(".thumb-4").click(function() {
-  $(".screen-4").toggle();
-  $(".thumb-4").toggle();
-});
-$(".screen-4").click(function() {
-  $(".screen-4").toggle();
-  $(".thumb-4").toggle();
-});
-$(".thumb-5").click(function() {
-  $(".screen-5").toggle();
-  $(".thumb-5").toggle();
-});
-$(".screen-5").click(function() {
-  $(".screen-5").toggle();
-  $(".thumb-5").toggle();
-});
-$(".thumb-6").click(function() {
-  $(".screen-6").toggle();
-  $(".thumb-6").toggle();
-});
-$(".screen-6").click(function() {
-  $(".screen-6").toggle();
-  $(".thumb-6").toggle();
-});
-$(".thumb-7").click(function() {
-  $(".screen-7").toggle();
-  $(".thumb-7").toggle();
-});
-$(".screen-7").click(function() {
-  $(".screen-7").toggle();
-  $(".thumb-7").toggle();
-});
-$(".thumb-8").click(function() {
-  $(".screen-8").toggle();
-  $(".thumb-8").toggle();
-});
-$(".screen-8").click(function() {
-  $(".screen-8").toggle();
-  $(".thumb-8").toggle();
-});
-$(".thumb-9").click(function() {
-  $(".screen-9").toggle();
-  $(".thumb-9").toggle();
-});
-$(".screen-9").click(function() {
-  $(".screen-9").toggle();
-  $(".thumb-9").toggle();
-});
-$(".thumb-10").click(function() {
-  $(".screen-10").toggle();
-  $(".thumb-10").toggle();
-});
-$(".screen-10").click(function() {
-  $(".screen-10").toggle();
-  $(".thumb-10").toggle();
-});
-$(".thumb-11").click(function() {
-  $(".screen-11").toggle();
-  $(".thumb-11").toggle();
-});
-$(".screen-11").click(function() {
-  $(".screen-11").toggle();
-  $(".thumb-11").toggle();
-});
-$(".thumb-12").click(function() {
-  $(".screen-12").toggle();
-  $(".thumb-12").toggle();
-});
-$(".screen-12").click(function() {
-  $(".screen-12").toggle();
-  $(".thumb-12").toggle();
-});
-$(".thumb-13").click(function() {
-  $(".screen-13").toggle();
-  $(".thumb-13").toggle();
-});
-$(".screen-13").click(function() {
-  $(".screen-13").toggle();
-  $(".thumb-13").toggle();
-});
-$(".thumb-14").click(function() {
-  $(".screen-14").toggle();
-  $(".thumb-14").toggle();
-});
-$(".screen-14").click(function() {
-  $(".screen-14").toggle();
-  $(".thumb-14").toggle();
-});
+window.onload = function () {
+  var thumbs = document.querySelectorAll('.thumb');
+
+  for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].addEventListener('click', function(e) {
+      e.target.classList.toggle('screen');
+      e.target.classList.toggle('thumb');
+    });
+  }
+};
